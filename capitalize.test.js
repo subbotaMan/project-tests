3ca5626b38c9af467eeb8fedbcbc4ec2ccdd1f52
subbotaMan <tabolskysergei@yandex.ru>
2025-06-02 15:23:27 +0300

    jest add and setting

--- capitalize.test.js
+++
@@ -1,8 +0,0 @@
-import { strict as assert } from "node:assert";
-import capitalize from "./capitalize.js";
-
-assert.equal(capitalize("hello"), "Hello");
-assert.equal(capitalize(""), "");
-assert.equal(capitalize(123), "Error: data type not string");
-
-console.log("Тесты завершились успешно");
